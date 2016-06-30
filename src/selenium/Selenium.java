7f8ac246df23a044b7c1360c1379babd6710a0ab
Yurii Veremiienko <i0638143049@gmail.com>
2016-06-30 23:09:22 +0300

    test

--- src/selenium/Selenium.java
+++ src/selenium/Selenium.java
@@ -3,8 +3,11 @@ package selenium;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class Selenium {
@@ -12,12 +15,33 @@ public class Selenium {
 
     public static void main(String[] args) {
 //        WebDriver driver = new FirefoxDriver();
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Java\\Desktop\\Selenium_Java\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Leon\\Desktop\\Selenium_Java\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://old.qalight.com.ua/zapisatsya-na-kursy/");
 
-        driver.get("http://www.seleniumhq.org/");
-        driver.findElement(By.xpath("//a[@title=\"Get Selenium\"]")).click();
-        driver.findElement(By.cssSelector("[title=\"Return to Selenium home page\"]")).click();
+        WebElement surname = driver.findElement(By.xpath(".//*[@id='wpcf7-f637-p638-o1']/form/p[2]/span/input"));
+        surname.sendKeys("rklgshdkgshdkf");
+        surname.submit();
+
+        boolean isElementPresent = driver.findElement(By.xpath(".//*[@id='wpcf7-f637-p638-o1']/form/p[8]/span/span")).isDisplayed();
+
+
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+
+
+
+//        String url = driver.getCurrentUrl();
+//        String handle = driver.getWindowHandle();
+//        String source = driver.getPageSource();
+//
+//        System.out.println(url);
+//        System.out.println(handle);
+//        System.out.println(source);
+
+//        driver.close(); //close current tab/window
+//        driver.quit(); //end session
     }
 
 }
